658e48fe28430dad359b54f5dbbb47458cf8dd28
carrych <romkacarry@gmail.com>
2026-02-16 12:15:25 +0200

    ✨ Add GraphQL API for `Stocks`

---
+++ src/graphql/stocks/resolvers/stocks.resolver.ts
@@ -0,0 +1,31 @@
+import { Resolver, Query, Args, Int, ResolveField, Parent } from '@nestjs/graphql';
+import { StocksService } from '../../../stocks/stocks.service';
+import { StockType } from '../types/stock.type';
+import { ProductType } from '../../orders/types/product.type';
+import { ProductLoader } from '../../orders/loaders/product.loader';
+import { Stock } from '../../../stocks/entities/stock.entity';
+
+@Resolver(() => StockType)
+export class StocksResolver {
+  constructor(
+    private readonly stocksService: StocksService,
+    private readonly productLoader: ProductLoader,
+  ) {}
+
+  @Query(() => [StockType], { name: 'stocks' })
+  async getStocks(): Promise<StockType[]> {
+    return this.stocksService.findAll();
+  }
+
+  @Query(() => StockType, { name: 'stock' })
+  async getStock(
+    @Args('productId', { type: () => Int }) productId: number,
+  ): Promise<StockType> {
+    return this.stocksService.findByProductId(productId);
+  }
+
+  @ResolveField('product', () => ProductType)
+  async getProduct(@Parent() stock: Stock): Promise<ProductType> {
+    return this.productLoader.load(Number(stock.productId));
+  }
+}
