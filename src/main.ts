ee17386f67534b4e8dac00b0c158efe716299b70
carrych <romkacarry@gmail.com>
2026-03-06 11:08:18 +0200

    ✨ Add global `ValidationPipe` to application bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: true,
   });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
 
   await app.listen(process.env.PORT ?? 3000);
 
