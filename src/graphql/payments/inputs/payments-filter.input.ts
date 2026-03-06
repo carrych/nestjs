cac60ff1479eefc357f3aaf06bb1c732dfec032e
carrych <romkacarry@gmail.com>
2026-03-06 11:08:14 +0200

    🐛 Fix `ValidationPipe` rejecting undecorated filter input fields

--- src/graphql/payments/inputs/payments-filter.input.ts
+++ src/graphql/payments/inputs/payments-filter.input.ts
@@ -1,11 +1,16 @@
 import { InputType, Field, Int } from '@nestjs/graphql';
+import { IsEnum, IsInt, IsOptional } from 'class-validator';
 import { PaymentStatus } from '../enums/payment-enums';
 
 @InputType()
 export class PaymentsFilterInput {
   @Field(() => Int, { nullable: true })
+  @IsOptional()
+  @IsInt()
   orderId?: number;
 
   @Field(() => PaymentStatus, { nullable: true })
+  @IsOptional()
+  @IsEnum(PaymentStatus)
   status?: PaymentStatus;
 }
