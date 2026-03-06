a3c2b234dabd02f5925afd8eaf58eacd72826a82
carrych <romkacarry@gmail.com>
2026-03-06 11:08:14 +0200

    🐛 Fix `ValidationPipe` rejecting undecorated filter input fields

--- src/graphql/products/inputs/products-filter.input.ts
+++ src/graphql/products/inputs/products-filter.input.ts
@@ -1,10 +1,15 @@
 import { InputType, Field } from '@nestjs/graphql';
+import { IsOptional, IsString } from 'class-validator';
 
 @InputType()
 export class ProductsFilterInput {
   @Field({ nullable: true })
+  @IsOptional()
+  @IsString()
   brand?: string;
 
   @Field({ nullable: true, description: 'Search by product name' })
+  @IsOptional()
+  @IsString()
   search?: string;
 }
