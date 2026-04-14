bb54cff626c70d5b98f6176d100ab8aaddf8d74b
carrych <romkacarry@gmail.com>
2026-04-14 15:35:23 +0300

    👔 Log `role_changed` and `sessions_revoked` admin events

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { AuditLogsModule } from '../audit-logs/audit-logs.module';
 import { User } from './entities/user.entity';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [AuditLogsModule, TypeOrmModule.forFeature([User])],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
